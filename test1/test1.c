74da86032ef62c910c3a55144580930a983966c6
HappyStudyHum <1282072004@qq.com>
2023-06-16 23:22:16 +0800

    提交代码

---
+++ test1/test1.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	printf("HelloWord!\n");
+	printf("请输入a的整数值：");
+	scanf("%d",&a);
+	printf("\na=%d",a);
+	return 0;
+}
